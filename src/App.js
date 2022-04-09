7c930ae0453cb96ecb32a7197e09308d105f92b8
saket105 <saketcool35@gmail.com>
2022-04-10 01:29:06 +0530

    prenavbar and navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
 import PreNavbar from "./components/PreNavbar";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
+
 
 function App() {
-  return <PreNavbar />;
+  return (
+    <Router>
+      <PreNavbar />
+      <Navbar />
+    </Router>
+  );
 }
 
 export default App;
