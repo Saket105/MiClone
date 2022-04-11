479b649bdf96aa0e60985f608134dfae0bd249a9
saket105 <saketcool35@gmail.com>
2022-04-11 22:32:22 +0530

    offer section done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import PreNavbar from "./components/PreNavbar";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
 import Slider from "./components/Slider";
+import Offers from "./components/Offers";
 const data = require("./data/data.json");
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <PreNavbar />
       <Navbar />
       <Slider start={data.banner.start} />
+      <Offers offer={data.offer} />
     </Router>
   );
 }
