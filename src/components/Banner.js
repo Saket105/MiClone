77ec5e82f8759a58001852c5bfdbf82f5d85d296
saket105 <saketcool35@gmail.com>
2022-04-17 19:59:37 +0530

    banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { CarouselItem } from "react-bootstrap";
 
 const Banner = ({ banner }) => {
   return (
